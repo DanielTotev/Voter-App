710a74cbe2e06a7c55f13cfe45254bb1fe5164e6
DanielTotev <dtotev1@gmail.com>
2018-08-05 16:17:54 +0300

    Changed connection-string at settings.js

--- server/config/settings.js
+++ server/config/settings.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 let rootPath = path.normalize(path.join(__dirname, '/../../'));
 let port = process.env.PORT || 1337;
-let db = 'mongodb://localhost:27017/gameShop';
+let db = 'mongodb://localhost:27017/votingApp';
 
 module.exports = {
     development: {
